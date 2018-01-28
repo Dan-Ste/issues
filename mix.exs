890b753be5f1ea7678e29a7eb6c896e8c37d389f
Dan Steshenko <dan.steshenko@gmail.com>
2018-01-28 16:20:03 +0300

    Fetch issues from github

--- mix.exs
+++ mix.exs
@@ -6,7 +6,7 @@ defmodule Issues.Mixfile do
       app: :issues,
       version: "0.1.0",
       elixir: "~> 1.5",
-      start_permanent: Mix.env == :prod,
+      start_permanent: Mix.env() == :prod,
       deps: deps()
     ]
   end
@@ -21,6 +21,8 @@ defmodule Issues.Mixfile do
   # Run "mix help deps" to learn about dependencies.
   defp deps do
     [
+      {:httpoison, "~> 1.0.0"},
+      {:poison, "~> 3.1"}
       # {:dep_from_hexpm, "~> 0.3.0"},
       # {:dep_from_git, git: "https://github.com/elixir-lang/my_dep.git", tag: "0.1.0"},
     ]
